acc5a8500f2c63916c0f3fe57a9087c594acd86c
Saif Al-Diresee <saif.aldiresee@gmail.com>
2025-02-05 21:47:02 +0100

    Add variable playerMove for player

--- js/script.js
+++ js/script.js
@@ -1,3 +1,8 @@
+let computerMove = 'kamień'; // Zmienna dla ruchu komputera
+let playerMove = 'papier'; // Zmienna dla ruchu gracza
+
+printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to ' + playerMove + ', to wygrywasz!');
+
 function printMessage(msg){
 	let div = document.createElement('div');
 	div.innerHTML = msg;
