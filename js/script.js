0dae915d052d5b94f0345059e6872bd931399d89
Saif Al-Diresee <saif.aldiresee@gmail.com>
2025-02-06 16:37:22 +0100

    complete logic and show result

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
-let computerMove = 'kamień'; // Zmienna dla ruchu komputera
-let playerMove = 'papier'; // Zmienna dla ruchu gracza
 
-printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to ' + playerMove + ', to wygrywasz!');
+let randomNumber = Math.floor(Math.random() * 3 + 1);
+let computerMove = randomNumber; 
+let playerMove = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+
+
 
 function printMessage(msg){
 	let div = document.createElement('div');
@@ -9,6 +11,24 @@ function printMessage(msg){
 	document.getElementById('messages').appendChild(div);
 }
 
-function clearMessages(){
-	document.getElementById('messages').innerHTML = '';
+if (playerMove < 1 || playerMove > 3) {
+    printMessage('Nieznany ruch! Proszę wybierz 1, 2 lub 3.');
+} else {
+    printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to ' + playerMove + ', to wygrywasz!');
+
+    if (computerMove === playerMove) {
+        printMessage('Remis!');
+    } else if (computerMove === 1 && playerMove === 2) {
+        printMessage('Ty wygrywasz!');
+    } else if (computerMove === 1 && playerMove === 3) {
+        printMessage('Ty przegrywasz!');
+    } else if (computerMove === 2 && playerMove === 1) {
+        printMessage('Ty przegrywasz!');
+    } else if (computerMove === 2 && playerMove === 3) {
+        printMessage('Ty wygrywasz!');
+    } else if (computerMove === 3 && playerMove === 1) {
+        printMessage('Ty wygrywasz!');
+    } else if (computerMove === 3 && playerMove === 2) {
+        printMessage('Komputer wygrywa!');
+    }
 }
